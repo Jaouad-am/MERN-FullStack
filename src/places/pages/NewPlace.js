c89f6cfddc0d350050679ce8eb29d174644ab013
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2020-06-08 07:14:57 +0100

    setting up folders, pages and routes

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,5 @@
+function NewPlace() {
+  return <h1>new place page</h1>;
+}
+
+export default NewPlace;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,5 @@
+const Users = () => {
+  return <h2>users page!</h2>;
+};
+
+export default Users;
